61acf35885e0045c422bd8c625b1dc4b1c477ba0
Sabuhi Gasimzada <sabuhi.qasimzada2021@khazar.org>
2025-01-28 14:54:38 +0400

    *

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='file-downloading-tool',
-    version='0.1.0',
+    name='speed-test-tool',
+    version='1.0.1',
     description='A simple CLI tool for downloading files with progress tracking',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
